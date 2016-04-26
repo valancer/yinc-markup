4ccd95ffbbc2fc219155f534147ef3ed7b17259b
valancer <developer.github@valancer.com>
2016-04-26 12:15:26 +0900

    기업 목록 / 상세 작업 중

--- sources/assets/scripts/common.js
+++ sources/assets/scripts/common.js
@@ -13,8 +13,63 @@
 
 
 
-
+var navigationHeight = 44;
 $(document).ready(function (e) {
+	Company.init();
 });
+
+
+/* Company */
+var Company = (function ($) {
+	var scope,
+		$companyContainer,
+		$companyHeader,
+		$tabs,
+		$tabContents,
+		init = function () {
+			$companyContainer = $('.contents.company');
+			$companyHeader = $companyContainer.find('> .company-info');
+			$tabs = $companyContainer.find('.tabs');
+			$tabContents = $companyContainer.find('.tab-contents');
+
+			initLayout();
+			initEvent();
+		};//end init
+
+	function initLayout() {
+	}
+
+	function initEvent() {
+		$(window).on('scroll', function(e) { 
+			if( $('body').hasClass('safari') ) {
+				console.log('safari');
+				return;
+			}
+
+			var scrollTop = $(this).scrollTop() + navigationHeight;
+			var headerHeight = $companyHeader.outerHeight();
+			
+			console.log('scrollTop : ' + scrollTop + ', headerHeight : ' + headerHeight);
+			if( scrollTop >= headerHeight ) {
+				$tabs.addClass("fixed");
+				$tabContents.css('marginTop', navigationHeight);
+			} else {
+				$tabs.removeClass("fixed");
+				$tabContents.css('marginTop', 0);
+			}
+		});
+	}
+
+	return {
+		init: function () {
+			scope = this;
+
+			init();
+		}
+	};
+}(jQuery));
+
+
+
 
 
